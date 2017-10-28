6e0847afcf23bc37b49301c3ab6e4de493538325
salma-abd-elaziz <salma-abd-elaziz@users.noreply.github.com>
2017-10-28 19:03:14 +0200

    trivail, fails in case of rows of same tength but not = 1

---
+++ src/leetcode/BreakWall.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+public class BreakWall {
+
+	public int leastBricks(ArrayList<ArrayList<Integer>> wall) {
+		int length = 0, crossedBreakesNo = Integer.MAX_VALUE;
+		// Find the length of the wall.
+		for (int i = 0; i < wall.get(0).size(); i++) {
+			length += wall.get(0).get(i);
+		}
+		// in case the wall was on the special case [[l],[l],....,[l]].
+		boolean found = false;
+		for (int j = 1;!found && j < wall.size() ; j++) {
+			if (wall.get(j).size() != wall.get(j-1).size()) found = true;
+		}
+		
+		if (!found) {
+			return wall.size();
+		}
+		// For each unit in the length, try to draw the line.
+		for (int i = 1; i < length; i++) {
+			int lineCrossedBreakesNo = 0;
+			for (int j = 0; j < wall.size(); j++) {
+				ArrayList<Integer> row = wall.get(j);
+				int sum = 0;
+				for (int k = 0; k < row.size(); k++) {
+					sum += row.get(k);
+					if (sum == i)
+						break; // Not crossed.
+					if (sum > i) { // crossed.
+						lineCrossedBreakesNo++;
+						break;
+					}
+				}
+			}
+			// After finding the no of crossed breaks at line i.
+			if (crossedBreakesNo > lineCrossedBreakesNo)
+				crossedBreakesNo = lineCrossedBreakesNo;
+		}
+		return crossedBreakesNo;
+	}
+
+}
---
+++ src/leetcode/Launcher.java
@@ -0,0 +1,80 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+		BreakWall s = new BreakWall();
+		ArrayList<ArrayList<Integer>> wall = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> d = new ArrayList<Integer>();
+		d.add(1);
+		d.add(2);    
+		d.add(2);
+		d.add(1);
+		
+		wall.add(d);
+		d = new ArrayList<Integer>();
+		
+		d.add(3);
+		d.add(1);
+		d.add(2);
+		
+		wall.add(d);
+		d = new ArrayList<Integer>();
+		
+		d.add(1);
+		d.add(3);
+		d.add(2);
+		 
+		wall.add(d);
+		d = new ArrayList<Integer>();
+		
+		
+		
+		d.add(2);
+		d.add(4);
+		
+		wall.add(d);
+		d = new ArrayList<Integer>();
+		
+		d.add(3);
+		d.add(1);
+		d.add(2);
+		
+		wall.add(d);
+		d = new ArrayList<Integer>();
+		
+		d.add(1);
+		d.add(3);
+		d.add(1);
+		d.add(1);
+		
+		
+		wall.add(d);
+		d = new ArrayList<Integer>();
+		
+		System.out.println(s.leastBricks(wall));
+
+		
+		
+		
+		
+		wall = new ArrayList<ArrayList<Integer>>();
+		
+		d.add(100000000);
+		wall.add(d);
+		
+		d = new ArrayList<Integer>();
+		d.add(100000000);
+		wall.add(d);
+		
+		d = new ArrayList<Integer>();
+		d.add(100000000);
+		wall.add(d);
+		
+		d = new ArrayList<Integer>();
+		
+		System.out.println(s.leastBricks(wall));
+	}
+}
