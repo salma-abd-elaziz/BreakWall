08fb8db7d2f19b57393d88338ac673e5dd1f3a9c
salma-abd-elaziz <salma-abd-elaziz@users.noreply.github.com>
2017-10-28 19:20:23 +0200

    f

--- src/leetcode/BreakWall.java
+++ src/leetcode/BreakWall.java
@@ -7,23 +7,20 @@ import java.util.Set;
 public class BreakWall {
 
 	public int leastBricks(ArrayList<ArrayList<Integer>> wall) {
-		int length = 0, crossedBreakesNo = Integer.MAX_VALUE;
-		// Find the length of the wall.
-		for (int i = 0; i < wall.get(0).size(); i++) {
-			length += wall.get(0).get(i);
+		int crossedBreakesNo =Integer.MAX_VALUE;
+		// To know where to try to put a line.
+		Set<Integer> placeToTry= new HashSet<Integer>();
+		for (int j = 0; j < wall.size(); j++) {
+			ArrayList<Integer> row = wall.get(j);
+			int sum = 0;
+			for (int k = 0; k < row.size() - 1; k++) {
+				sum += row.get(k);
+				placeToTry.add(sum);
+			}
 		}
-		// in case the wall was on the special case [[l],[l],....,[l]].
-		boolean found = false;
-		for (int j = 1;!found && j < wall.size() ; j++) {
-			if (wall.get(j).size() != wall.get(j-1).size()) found = true;
-		}
-		
-		if (!found && wall.get(0).size() == 1) {
-			
-			return wall.size();
-		}
+
 		// For each unit in the length, try to draw the line.
-		for (int i = 1; i < length; i++) {
+		for (Integer i : placeToTry){
 			int lineCrossedBreakesNo = 0;
 			for (int j = 0; j < wall.size(); j++) {
 				ArrayList<Integer> row = wall.get(j);
@@ -42,7 +39,8 @@ public class BreakWall {
 			if (crossedBreakesNo > lineCrossedBreakesNo)
 				crossedBreakesNo = lineCrossedBreakesNo;
 		}
-		return crossedBreakesNo;
+		// in case the wall was on the special case [[l],[l],....,[l]].
+		return crossedBreakesNo == Integer.MAX_VALUE? wall.size() : crossedBreakesNo;
 	}
 
 }
