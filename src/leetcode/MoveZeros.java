ee1ac398ea2b11627f0a75d808998a52301a211e
salma-abd-elaziz <salma-abd-elaziz@users.noreply.github.com>
2017-11-16 08:11:45 +0200

    move zeros mor efficient

--- src/leetcode/MoveZeros.java
+++ src/leetcode/MoveZeros.java
@@ -7,21 +7,13 @@ public class MoveZeros {
 	public void moveZeroes(int[] nums) {
 		if (nums == null)
 			return;
-		// p0 used to find the first zero element, p1 always on the first
-		// nonzero
-		// element and it must be bigger than p0.
-		int p0 = 0, p1 = 0;
-		// find p0 and p1 initially.
 
-		while (p0 >= 0 && p0 < nums.length && p1 >= 0 && p1 < nums.length) {
-			while (p0 < nums.length && nums[p0] != 0)
-				p0++;
-			p1 = p0;
-			while (p1 < nums.length && nums[p1] == 0)
-				p1++;
-			if (p1 >= nums.length)
-				break; // Finished.
-			swap(nums, p0, p1);
+		int p0 = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				swap(nums, i, p0++);
+			}
 		}
 	}
 
